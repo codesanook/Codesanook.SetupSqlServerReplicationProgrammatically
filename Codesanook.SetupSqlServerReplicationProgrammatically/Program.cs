991493c67a7c0609425e3a807247600777a0f2cf
aaronamm <theeranitp@gmail.com>
2019-08-16 20:21:25 +0700

    Add required DLL for SQL Server replication

---
+++ Codesanook.SetupSqlServerReplicationProgrammatically/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Replication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesanook.SetupSqlServerReplicationProgrammatically
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sc = new ServerConnection("localhost");
+            new PublicationMonitor();
+            sc.Disconnect();
+        }
+    }
+}
